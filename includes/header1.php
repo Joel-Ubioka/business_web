e3cb09f4e73ae91d7ab4d4dd31d32b5be3804d88
morrhtechsolutions <joelubioka@gmail.com>
2022-04-27 00:44:01 +0100

    registration

--- includes/header1.php
+++ includes/header1.php
@@ -3,6 +3,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Business_Website</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style.css?ver<?php echo microtime(); ?>">
     <script src="https://kit.fontawesome.com/27ae433552.js" crossorigin="anonymous"></script>
 </head>
--- register.php
+++ register.php
@@ -6,31 +6,40 @@
     <?php require "includes/header2.php"; ?>
     <div class="title">REGISTER</div>
     <div class="main_container">
-        <div class="side_bar">
-            <div class="product">
-                <img src="images/product/BITCOIN.jpg" alt="">
-                <h4>Bitcoin</h4>
-            </div>
-            <div class="product">
-                <img src="images/product/Binance coin.webp" alt="">
-                <h4>BNB</h4>
-            </div>
-            <div class="product">
-                <img src="images/eth.png" alt="">
-                <h4>Etherieum</h4>
-            </div>
 
-        </div>
         <div class="main_body">
             <div class="form_container">
                 <form action="" method="POST">
-                    <div class="form_control">
 
+                    <h2>Register</h2>
+
+                    <div class="form_control">
+                        <p>FULL NAME :</p>
+                        <input type="text" name="full_name" class="form_input" required>
+                    </div>
+
+                    <div class="form_control">
+                        <p>PHONE NUMBER :</p>
+                        <input type="tel" name="phone_number" class="form_input" required>
+                    </div>
+
+                    <div class="form_control">
+                        <p>EMAIL :</p>
+                        <input type="email" name="email" class="form_input" required>
+                    </div>
+
+                    <div class="form_control">
+                        <p>PASSWORD :</p>
+                        <input type="password" name="password" class="form_input" required>
+                    </div>
+
+                    <div class="form_control">
+                        <input type="submit" name="register" class="form_input" value="Register">
                     </div>
                 </form>
             </div>
-        </div>
-        <?php include "includes/footer.php"; ?>
+        </div><br><br>
+
 
 </body>
 
