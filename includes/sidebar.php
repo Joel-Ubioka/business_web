70549db1cca563d83355deccf3b96a317db4afaa
morrhtechsolutions <joelubioka@gmail.com>
2022-07-06 18:44:33 +0100

    blog-update

---
+++ includes/sidebar.php
@@ -0,0 +1,89 @@
+<h3 class="latest_news_head">LATEST NEWS</h3>
+
+
+<div class="post_container">
+
+        <div class="sidebar_post_img">
+                <img src="./images/blog1.png" alt="">
+        </div>
+        <div class="sidebar_post_content">
+                <h4>Heading of the blog post</h4>
+                <small>By Morrhtech Solutions, </small>
+                <small>2 hours ago</small>
+
+                <a href="single_news.php"><button class="sidebar_read_more">Read More</button></a>
+        </div>
+</div>
+
+
+<div class="post_container">
+
+        <div class="sidebar_post_img">
+                <img src="./images/blog1.png" alt="">
+        </div>
+        <div class="sidebar_post_content">
+                <h4>Heading of the blog post</h4>
+                <small>By Morrhtech Solutions, </small>
+                <small>2 hours ago</small>
+
+                <a href="single_news.php"><button class="sidebar_read_more">Read More</button></a>
+        </div>
+</div>
+
+
+<div class="post_container">
+
+        <div class="sidebar_post_img">
+                <img src="./images/blog1.png" alt="">
+        </div>
+        <div class="sidebar_post_content">
+                <h4>Heading of the blog post</h4>
+                <small>By Morrhtech Solutions, </small>
+                <small>2 hours ago</small>
+
+                <a href="single_news.php"><button class="sidebar_read_more">Read More</button></a>
+        </div>
+</div>
+
+
+<div class="post_container">
+
+        <div class="sidebar_post_img">
+                <img src="./images/blog1.png" alt="">
+        </div>
+        <div class="sidebar_post_content">
+                <h4>Heading of the blog post</h4>
+                <small>By Morrhtech Solutions, </small>
+                <small>2 hours ago</small>
+
+                <a href="single_news.php"><button class="sidebar_read_more">Read More</button></a>
+        </div>
+</div>
+
+
+<div class="post_container">
+
+        <div class="sidebar_post_img">
+                <img src="./images/blog1.png" alt="">
+        </div>
+        <div class="sidebar_post_content">
+                <h4>Heading of the blog post</h4>
+                <small>By Morrhtech Solutions, </small>
+                <small>2 hours ago</small>
+
+                <a href="single_news.php"><button class="sidebar_read_more">Read More</button></a>
+        </div>
+</div>
+
+<div class="quick_link_container">
+        <h3 class="quick_link_head">QUICK LINKS</h3>
+        <ul>
+                <li><a href="#">HOME</a></li>
+                <li><a href="#">CONTACT</a></li>
+                <li><a href="#">NEWS</a></li>
+                <li><a href="#">SPORT</a></li>
+                <li><a href="#">ABOUT US</a></li>
+        </ul>
+</div>
+
+</div>
--- single_news.php
+++ single_news.php
@@ -16,13 +16,13 @@
 
                 <div class="main_body">
 
-                        <div class="post_container">
+                        <div class="single_news_container">
 
                                 <div class="post_img">
                                         <img src="./images/blog1.png" alt="">
                                 </div>
-                                <div class="post_content">
-                                        <h2 class="blog_head">Heading of the blog post</h2>
+                                <div class="single_post_content">
+                                        <h1 class="single_news_head">Heading of the blog post</h1>
                                         <small>By Morrhtech Solutions, </small>
                                         <small>2 hours ago</small>
                                         <p>It is a long established fact that a reader will be distracted by the
@@ -42,19 +42,7 @@
                                                 by
                                                 accident, sometimes on purpose (injected humour and the like).
                                         </p>
-                                        <a href="single_news.php"><button class="read_more">Read More</button></a>
-                                </div>
-                        </div>
 
-                        <div class="post_container">
-
-                                <div class="post_img">
-                                        <img src="./images/blog1.png" alt="">
-                                </div>
-                                <div class="post_content">
-                                        <h2 class="blog_head">Heading of the blog post</h2>
-                                        <small>By Morrhtech Solutions, </small>
-                                        <small>2 hours ago</small>
                                         <p>It is a long established fact that a reader will be distracted by the
                                                 readable content of a page when looking at its layout. The point
                                                 of
@@ -72,20 +60,8 @@
                                                 by
                                                 accident, sometimes on purpose (injected humour and the like).
                                         </p>
-                                        <a href="single_news.php"><button class="read_more">Read More</button></a>
-                                </div>
-                        </div>
 
 
-                        <div class="post_container">
-
-                                <div class="post_img">
-                                        <img src="./images/blog1.png" alt="">
-                                </div>
-                                <div class="post_content">
-                                        <h2 class="blog_head">Heading of the blog post</h2>
-                                        <small>By Morrhtech Solutions, </small>
-                                        <small>2 hours ago</small>
                                         <p>It is a long established fact that a reader will be distracted by the
                                                 readable content of a page when looking at its layout. The point
                                                 of
@@ -103,20 +79,8 @@
                                                 by
                                                 accident, sometimes on purpose (injected humour and the like).
                                         </p>
-                                        <a href="single_news.php"><button class="read_more">Read More</button></a>
-                                </div>
-                        </div>
 
 
-                        <div class="post_container">
-
-                                <div class="post_img">
-                                        <img src="./images/blog1.png" alt="">
-                                </div>
-                                <div class="post_content">
-                                        <h2 class="blog_head">Heading of the blog post</h2>
-                                        <small>By Morrhtech Solutions, </small>
-                                        <small>2 hours ago</small>
                                         <p>It is a long established fact that a reader will be distracted by the
                                                 readable content of a page when looking at its layout. The point
                                                 of
@@ -134,16 +98,94 @@
                                                 by
                                                 accident, sometimes on purpose (injected humour and the like).
                                         </p>
-                                        <a href="single_news.php"><button class="read_more">Read More</button></a>
+
+
+                                        <p>It is a long established fact that a reader will be distracted by the
+                                                readable content of a page when looking at its layout. The point
+                                                of
+                                                using Lorem Ipsum is that it has a more-or-less normal
+                                                distribution of
+                                                letters, as opposed to using 'Content here, content here',
+                                                making it
+                                                look like readable English. Many desktop publishing packages and
+                                                web
+                                                page editors now use Lorem Ipsum as their default model text,
+                                                and a
+                                                search for 'lorem ipsum' will uncover many web sites still in
+                                                their
+                                                infancy. Various versions have evolved over the years, sometimes
+                                                by
+                                                accident, sometimes on purpose (injected humour and the like).
+                                        </p>
+
+
+                                        <p>It is a long established fact that a reader will be distracted by the
+                                                readable content of a page when looking at its layout. The point
+                                                of
+                                                using Lorem Ipsum is that it has a more-or-less normal
+                                                distribution of
+                                                letters, as opposed to using 'Content here, content here',
+                                                making it
+                                                look like readable English. Many desktop publishing packages and
+                                                web
+                                                page editors now use Lorem Ipsum as their default model text,
+                                                and a
+                                                search for 'lorem ipsum' will uncover many web sites still in
+                                                their
+                                                infancy. Various versions have evolved over the years, sometimes
+                                                by
+                                                accident, sometimes on purpose (injected humour and the like).
+                                        </p>
+
+
+                                        <p>It is a long established fact that a reader will be distracted by the
+                                                readable content of a page when looking at its layout. The point
+                                                of
+                                                using Lorem Ipsum is that it has a more-or-less normal
+                                                distribution of
+                                                letters, as opposed to using 'Content here, content here',
+                                                making it
+                                                look like readable English. Many desktop publishing packages and
+                                                web
+                                                page editors now use Lorem Ipsum as their default model text,
+                                                and a
+                                                search for 'lorem ipsum' will uncover many web sites still in
+                                                their
+                                                infancy. Various versions have evolved over the years, sometimes
+                                                by
+                                                accident, sometimes on purpose (injected humour and the like).
+                                        </p>
+
+
+                                        <p>It is a long established fact that a reader will be distracted by the
+                                                readable content of a page when looking at its layout. The point
+                                                of
+                                                using Lorem Ipsum is that it has a more-or-less normal
+                                                distribution of
+                                                letters, as opposed to using 'Content here, content here',
+                                                making it
+                                                look like readable English. Many desktop publishing packages and
+                                                web
+                                                page editors now use Lorem Ipsum as their default model text,
+                                                and a
+                                                search for 'lorem ipsum' will uncover many web sites still in
+                                                their
+                                                infancy. Various versions have evolved over the years, sometimes
+                                                by
+                                                accident, sometimes on purpose (injected humour and the like).
+                                        </p>
+
+
                                 </div>
                         </div>
+
+
 
 
                 </div>
                 <div class="sidebar">
                         <?php require "includes/sidebar.php"; ?>
                 </div>
-
         </div>
 
         <?php include "includes/footer.php"; ?>
